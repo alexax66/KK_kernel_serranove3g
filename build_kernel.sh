91bc203e2a523d3df9be81e7f10b8ae8b828ea06
alexax66 <alexax66@gmail.com>
2015-06-14 14:01:34 +0000

    Set new kernel version

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,3 +1,4 @@
+# make clean && make mrproper 
 mkdir output
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_serranove3g_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j5 -C $(pwd) O=output
