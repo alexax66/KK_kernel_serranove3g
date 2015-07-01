cbe739aea80e1ef56f66842a17ed8e07cb834f78
alexax66 <alexax66@gmail.com>
2015-07-01 19:56:46 +0000

    fix build_dtb.sh for making dt.img v2

--- build_dtb.sh
+++ build_dtb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./dtbTool -o ./output/arch/arm/boot/dt.img -s 2048 -p ./output/scripts/dtc/ ./output/arch/arm/boot/dts/ -v
+./dtbTool -2 -o ./output/arch/arm/boot/dt.img -s 2048 -p ./output/scripts/dtc/ ./output/arch/arm/boot/dts/ -v
