30df65d0e0a72997ed5483ec7ebd4575adcac538
alexax66 <alexax66@gmail.com>
2015-07-06 11:46:58 +0000

    build_kernel.sh: Add dirctory for modules

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,3 +3,11 @@ mkdir output
 make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_serranove3g_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -j5 -C $(pwd) O=output
 cp $(pwd)/output/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+
+if [ ! -d $(pwd)/output/modules ];
+    then
+        mkdir $(pwd)/output/modules;
+    fi
+find $(pwd)/output -name '*.ko' -exec cp -v {} $(pwd)/output/modules \;
+
+exit
